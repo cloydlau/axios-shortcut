ec08d7caaef8a54675dd529f444e277d96c77f52
cloydlau <253055734@qq.com>
2022-12-11 23:08:45 +0800

    fix: preview is actually triggered instead of download if the browser recognizes that kind of file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import createAxiosShortcut from './axios-shortcut'
 
 export default createAxiosShortcut
+  DOWNLOAD: async (url: string, fileName = '') => {
+    // 如果是浏览器支持预览的文件会优先预览，否则才会下载
+    // window.open(url + stringify(params, { addQueryPrefix: true }))
+
+    const a = document.createElement('a')
+    a.style.display = 'none'
+    a.href = URL.createObjectURL(await fetch(url).then(res => res.blob()))
+    a.download = fileName
+    document.body.appendChild(a)
+    a.click()
+    URL.revokeObjectURL(a.href)
+    document.body.removeChild(a)
+    return Promise.resolve()
+  },
